17929fdd8737d1d33962f83306f30657ee2b5344
turanuku <yarune@proton.me>
2024-12-30 01:59:20 +0800

    Site updated: 2024-12-30 01:59:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/26/测试/","2024/12/26/FCEUX模拟器安装运行FC红白机游戏/","2024/12/26/hello-world/"];function toRandomPost(){
+var posts=["2024/12/26/FCEUX模拟器安装运行FC红白机游戏/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
